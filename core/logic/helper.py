e597f41b1133801eddbbb4d4773c51fe0b9ce056
nivedhak1110 <62648927+nivedhak1110@users.noreply.github.com>
2020-05-13 16:23:42 +0530

    Update helper.py

--- core/logic/helper.py
+++ core/logic/helper.py
@@ -1,36 +1,121 @@
+import mysql.connector
 
-"""
-1 Method to read and verify user input
-"""
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="",
+    database="signup"
+)
+
+
+def check(request):
+    print("Inside check")
+    #email_id = ''
+    email_id = request.form.get('uname')
+    passwd = request.form.get('password')
+    print(email_id)
+    print(passwd)
+    return "success"
+
+# Method to read and verify user input
 def read_input(request):
-    username = request.form.get('uname')
-    password = request.form.get('password')
-    print(username)
-    print(password)
-    # login is success if usernme is master , password is madam
-    if username == "master" and password == "madam":
-        print ("Login is success")
+
+    print("Inside readInput")
+    email_id = request.form.get('uname')
+    passwd = request.form.get('password')
+    print(email_id)
+    print(passwd)
+    #status = "success"
+    status =  login_verify(email_id, passwd)
+    return status
+ """  except Exception as exception:
+        print("Error ")
+        print(exception.args[0])"""
+
+
+# verify login details
+def login_verify(email_id, passwd):
+    mycursor = mydb.cursor()
+    sql = "select password from signup where email= '" + email_id + "'"
+    mycursor.execute(sql)
+    myresult = str(mycursor.fetchone()[0])
+    print(myresult)
+    if (passwd == myresult):
+        print("login success")
+        mycursor.close()
+        mydb.commit()
+        print_all()
         return "success"
     else:
+        print("login failed")
         return "failed"
-    # method to read and disp user input
-def disp_input(request):
+
+
+# print login details
+def print_all():
+    print("Printing all DB data")
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM signup")
+    myresult = mycursor.fetchall()
+    mycursor.close()
+
+    for x in myresult:
+        print(x)
+
+
+# method to read  user input
+def get_input(request):
     fname = request.form.get('fname')
     lname = request.form.get('lname')
     email = request.form.get('email')
     password = request.form.get('password')
     address = request.form.get('add')
-    print(fname)
-    print(lname)
-    print(email)
-    print(password)
-    print(address)
-    fp=open("signupdetails.txt" ,"a")
-    fp.write(fname)
-    fp.write(lname)
-    fp.write(email)
-    fp.write(password)
-    fp.write(address)
-    fp.close()
+    signup_insert(fname, lname, email, password, address)
+    return "success"
 
-    return ("printed details")
+
+# insert user signup  details
+def signup_insert(fname, lname, email, password, address):
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO signup VALUES(%s,%s,%s,%s,%s)"
+    val = (fname, lname, email, password, address)
+    mycursor.execute(sql, val)
+    mycursor.close()
+    mydb.commit()
+    print_all()
+
+
+"""
+def disp(request):
+
+    data={
+    "college":
+    {
+    "name":"ngpit", "depts":
+    {
+
+    "cse":[
+
+    {"name":"nive", "rollno":"18cs040"},
+    {"name":"priyu","rollno":"18cs046"}
+    ],
+
+    "IT":[
+    {"name":"mothika","rollno":"18IT036"},
+    {"name":"harini","rollno":"18IT053"}
+    ],
+
+    "ece":[
+    {"name":"miruthu","rollno":"18EC035"},
+    {"name":"yamuna","rollno":"18EC060"}
+    ]
+
+    }
+    }
+    }
+    with open("sample.json","a") as fp:
+        json.dump(data,fp)
+
+return("printed details")"""
+
+
